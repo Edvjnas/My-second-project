98faf87e6596f215cd53fa9d4efc5af5668748b1
Edvjnas <145874175+Edvjnas@users.noreply.github.com>
2023-10-13 14:31:13 +0300

    Update mylib.cpp
    Sutvarkytas kodas

--- mylib.cpp
+++ mylib.cpp
@@ -172,6 +172,136 @@ void klaida(int &a){
         while (a != "Vidurkis" && a != "VIDURKIS" && a != "vidurkis" && a != "Mediana" && a != "mediana" && a != "MEDIANA");
     }
 
+     void IvestiDuomenis(vector<Studentas>& studentai) {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> distribution(1, 10);
+     int s;
+        cout << "Norite pazymius suvesti ar nuskaityti juos is failo?(Rasykite 1, jei norite pazymius suvesti, ir 2, jei norite juos nuskaityti is failo)"<< endl;
+        cin >> s;
+        klaida2(s);
+        if (s==2){
+            ifstream input("2.txt");
+            if (!input.is_open()) {
+                cout << "Nepavyko atidaryti failo..." << endl;
+            }
+            else {
+                string pav;
+                    getline(input, pav);
+                    int ndsk = NamuDarbuSk(pav);
+
+    while (!input.eof()) {
+        Studentas naujasStudentas;
+        input >> naujasStudentas.vardas >> naujasStudentas.pavarde;
+        for(int i = 1; i <= ndsk; i++){
+            int nd;
+            input >> nd;
+            naujasStudentas.namuDarbai.push_back(nd);
+        }
+        input >> naujasStudentas.egzaminas;
+
+        naujasStudentas.galutinisBalas = SkaiciuotiGalutiniBala(naujasStudentas);
+        naujasStudentas.galutinisBalasM = SkaiciuotiGalutiniBalaM(naujasStudentas);
+        studentai.push_back(naujasStudentas);
+
+            }
+
+            input.close();
+            sort(studentai.begin(), studentai.end(), SortVardas);
+
+            ofstream output("4rez.txt");
+            output << "\nGalutiniai rezultatai:\n";
+            output << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(15) << "Galutinis balas(vid.)" << setw(20) << "Galutinis balas(med.)" << endl;
+            output << "----------------------------------------------------------------------------" << endl;
+            for (const Studentas& studentas : studentai) {
+                output << setw(15) << studentas.vardas << setw(15) << studentas.pavarde << fixed << setprecision(2) << setw(25) <<
+                    studentas.galutinisBalas << setprecision(2) << setw(20) << studentas.galutinisBalasM << endl;
+            }
+            output.close();
+        }
+    }
+
+    if (s==1){
+    while (true) {
+        Studentas naujasStudentas;
+
+        cout << "Iveskite studento varda: ";
+        cin >> naujasStudentas.vardas;
+
+        cout << "Iveskite studento pavarde: ";
+        cin >> naujasStudentas.pavarde;
+
+        naujasStudentas.namuDarbai.clear();
+
+        int nd;
+        cout << "Iveskite namu darbu tarpinius rezultatus (-1, jei baigete ir -2, jei norite, kad namu darbu balai butu sugeneruoti atsitiktinai): ";
+        while (true) {
+            cin >> nd;
+            klaida(nd);
+            if (nd == -1) {
+                break;
+            }
+            if (nd == -2){
+                int n=0;
+                cout << "Iveskite kiek namu darbu balu dar sugeneruoti: " << endl;
+                cin >> n;
+                klaida3(n);
+                for (int i = 1; i <= n; i++){
+                   nd = distribution(mt);
+                   naujasStudentas.namuDarbai.push_back(nd);
+                   cout << "(" << nd << ")" << endl;
+                }
+                break;
+                }
+            naujasStudentas.namuDarbai.push_back(nd);
+        }
+        cout << "Iveskite egzamino rezultata (-2, jei norite, kad ji sugeneruotu atsitiktinai): ";
+        cin >> naujasStudentas.egzaminas;
+        klaida1(naujasStudentas.egzaminas);
+        if(naujasStudentas.egzaminas == -2){
+            naujasStudentas.egzaminas = distribution(mt);
+            cout << "(" << naujasStudentas.egzaminas << ")" << endl;
+        }
+
+        naujasStudentas.galutinisBalas = SkaiciuotiGalutiniBala(naujasStudentas);
+        naujasStudentas.galutinisBalasM = SkaiciuotiGalutiniBalaM(naujasStudentas);
+
+        studentai.push_back(naujasStudentas);
+
+        cout << "Ar norite ivesti dar vieno studento duomenis? (Taip/Ne): ";
+        string pasirinkimas;
+        cin >> pasirinkimas;
+        klaida4(pasirinkimas);
+        if (pasirinkimas != "Taip" && pasirinkimas != "taip" && pasirinkimas != "TAIP") {
+            break;
+        }
+    }
+    sort(studentai.begin(), studentai.end(), SortVardas);
+    }
+    }
+
+
+void SpausdintiRezultatus(const vector<Studentas>& studentai) {
+    cout << "Norite kad isspausdintu studentu vidurkius ar medianas? (Vidurkis/Mediana): ";
+    string pasirinkimas2;
+    cin >> pasirinkimas2;
+    klaida5(pasirinkimas2);
+    cout << "\nGalutiniai rezultatai:\n";
+    if (pasirinkimas2 != "Vidurkis" && pasirinkimas2 != "vidurkis"){
+        cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(15) << "Galutinis balas(med)" << endl;
+        cout << "--------------------------------------------" << endl;
+        for (const Studentas& studentas : studentai) {
+            cout << setw(15) << studentas.vardas << setw(15) << studentas.pavarde << fixed << setprecision(2) << setw(15) << studentas.galutinisBalasM << endl;
+        }
+    }
+    else {
+        cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(15) << "Galutinis balas(vid)" << endl;
+        cout << "--------------------------------------------" << endl;
+        for (const Studentas& studentas : studentai) {
+        cout << setw(15) << studentas.vardas << setw(15) << studentas.pavarde << fixed << setprecision(2) << setw(15) << studentas.galutinisBalas << endl;
+        }
+    }
+}
     string GeneruotiVarda(int a) {
         return "Vardas" + to_string(a);
 }
@@ -179,13 +309,12 @@ void klaida(int &a){
         return " Pavarde" + to_string(a);
 }
 
-// Funkcija, kuri sugeneruoja atsitiktinius duomenis ir iraso i faila
-    void GeneruotiStudentus(int studentuSkaicius, const string& failoPavadinimas) {
+    void GeneruotiStudentus(int studentuSkaicius, const string& failoPavadinimas, int namuDarbuSkaicius) {
+        vector<Studentas> studentai;
         ofstream output(failoPavadinimas);
         std::random_device rd;
         std::mt19937 mt(rd());
         std::uniform_int_distribution<int> distribution(1, 10);
-        int namuDarbuSkaicius = 10;
 
         output << left << setw(20) << " Vardas" << setw(20) << "  Pavarde";
         for (int p = 1; p <= namuDarbuSkaicius; p++){
@@ -201,6 +330,8 @@ void klaida(int &a){
                 naujasStudentas.namuDarbai.push_back(distribution(mt));
             }
             naujasStudentas.egzaminas = distribution(mt);
+            naujasStudentas.galutinisBalas = SkaiciuotiGalutiniBala(naujasStudentas);
+            studentai.push_back(naujasStudentas);
             output << left << setw(20) << naujasStudentas.vardas << setw(20) << naujasStudentas.pavarde;
             for (const int& pazymys : naujasStudentas.namuDarbai) {
                 output << setw(10) << pazymys;
@@ -209,6 +340,10 @@ void klaida(int &a){
         }
         output.close();
     }
+
+
+
+
 
 
 
