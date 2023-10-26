75285b09ed15a718dfc62f7bee4e8eca8dbfca38
Edvjnas <145874175+Edvjnas@users.noreply.github.com>
2023-10-26 12:31:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 int main() {
-    //vector<Studentas> studentai;
-    //IvestiDuomenis(studentai);
-    //SpausdintiRezultatus(studentai);
+
+
+    /*vector<Studentas> studentai;
+    IvestiDuomenis(studentai);*/
+
     int n = 10;
-    int test = 5;
-    vector<Studentas> studentai;
+    int test = 3;
+    list<Studentas> studentai;
 
     /*GeneruotiStudentus(1000, "1000.txt", n);
     GeneruotiStudentus(10000, "10000.txt", n);
@@ -17,20 +19,21 @@ int main() {
     GeneruotiStudentus(1000000, "1000000.txt", n);
     GeneruotiStudentus(10000000, "10000000.txt", n);*/
 
-    //std::pair<vector<Studentas>, vector<Studentas>> sortedStudents = RikiuotiStudentus(studentai);
+    pair<list<Studentas>, list<Studentas>> sortedStudents = RikiuotiStudentus(studentai);
 
-    // 1000 irasu testu (5) vidurkis = 0.0320345
-    // 10000 irasu testu (5) vidurkis = 0.308776
-    // 100000 irasu testu (5) vidurkis = 2.84911
-    // 1000000 irasu testu (5) vidurkis = 32.4168
-    // 10000000 irasu testu (5) vidurkis = 161.129
+    // vector
+    // 1000 irasu testu (3) vidurkis = 0.0260391
+    // 10000 irasu testu (3) vidurkis = 0.186274
+    // 100000 irasu testu (3) vidurkis = 1.90762
+    // 1000000 irasu testu (3) vidurkis = 22.9662
+    // 10000000 irasu testu (3) vidurkis = 161.129
 
 
 
 
-    for (int i = 1000; i <= 10000000; i=i*10){
-        //chrono::duration<double> suma1 = chrono::duration<double>::zero();
-        //chrono::duration<double> vidurkis = chrono::duration<double>::zero();
+    for (int i = 1000; i <= 1000; i=i*10){
+        chrono::duration<double> suma1 = chrono::duration<double>::zero();
+        chrono::duration<double> vidurkis = chrono::duration<double>::zero();
     //for (int j = 1; j <= test; j++){
 
         chrono::duration<double> suma = chrono::duration<double>::zero();
@@ -39,19 +42,19 @@ int main() {
         auto end = std::chrono::high_resolution_clock::now();
         chrono::duration<double> diff = end-start;
         suma += diff;
-        cout << i << " studentu sugeneravimas ir spausdinimas i faila: " << diff.count() << endl;
+        cout << i << " studentu nuskaitymas is failo: " << diff.count() << endl;
 
             start = std::chrono::high_resolution_clock::now();
-            sort(studentai.begin(), studentai.end(), sortDidejant); //SortVardas, sortDidejant, sortMazejant
+            studentai.sort(sortDidejant); //SortVardas, sortDidejant, sortMazejant
             end = std::chrono::high_resolution_clock::now();
             diff = end - start;
             suma = suma + diff;
             cout << i << " studentu rusiavimas pagal parametra: " << diff.count() << endl;
 
                 start = std::chrono::high_resolution_clock::now();
-                pair<vector<Studentas>, vector<Studentas>> sortedStudents = RikiuotiStudentus(studentai);
-                vector<Studentas> genijai = sortedStudents.first;
-                vector<Studentas> vargsiukai = sortedStudents.second;
+                pair<list<Studentas>, list<Studentas>> sortedStudents = RikiuotiStudentus(studentai);
+                list<Studentas> genijai = sortedStudents.first;
+                list<Studentas> vargsiukai = sortedStudents.second;
                 end = std::chrono::high_resolution_clock::now();
                 diff = end - start;
                 suma = suma + diff;
@@ -84,4 +87,3 @@ int main() {
 
     return 0;
 }
-
