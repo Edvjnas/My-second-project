60ed36f9a8c2430bda5e26eeb4dead79e6dbeb36
Edvjnas <145874175+Edvjnas@users.noreply.github.com>
2023-11-10 16:16:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,43 +6,46 @@ using namespace std;
 int main() {
     //vector<Studentas> studentai;
     //IvestiDuomenis(studentai);
-    //SpausdintiRezultatus(studentai);
     int n = 10;
-    int test = 5;
+    int b,c;
     vector<Studentas> studentai;
 
-    /*GeneruotiStudentus(1000, "1000.txt", n);
-    GeneruotiStudentus(10000, "10000.txt", n);
-    GeneruotiStudentus(100000, "100000.txt", n);
-    GeneruotiStudentus(1000000, "1000000.txt", n);
-    GeneruotiStudentus(10000000, "10000000.txt", n);*/
+    cout << "Jei norite is naujo generuoti failus, rasykite 1" << endl;
+    cin >> b;
+    if (b == 1){
+        GeneruotiStudentus(1000, "1000.txt", n);
+        GeneruotiStudentus(10000, "10000.txt", n);
+        GeneruotiStudentus(100000, "100000.txt", n);
+        GeneruotiStudentus(1000000, "1000000.txt", n);
+        GeneruotiStudentus(10000000, "10000000.txt", n);
+    }
+    cout << "Pagal koki parametra norite rusiuoti? |1.Didejant | 2.Mazejant | 3.Vardas | (Rasykite tik skaiciuka)" << endl;
+    cin >> c;
 
-    //std::pair<vector<Studentas>, vector<Studentas>> sortedStudents = RikiuotiStudentus(studentai);
+    auto sortingFunction = sortDidejant;
+    if (c == 1){
+        sortingFunction = sortDidejant;
+    }
+    if (c == 2){
+        sortingFunction = sortMazejant;
+    }
+    if (c == 3){
+        sortingFunction= SortVardas;
+    }
 
-    // 1000 irasu testu (5) vidurkis = 0.0320345
-    // 10000 irasu testu (5) vidurkis = 0.308776
-    // 100000 irasu testu (5) vidurkis = 2.84911
-    // 1000000 irasu testu (5) vidurkis = 32.4168
-    // 10000000 irasu testu (5) vidurkis = 161.129
+    pair<vector<Studentas>, vector<Studentas>> sortedStudents = RikiuotiStudentus(studentai);
 
-
-
-
-    for (int i = 1000; i <= 10000000; i=i*10){
-        //chrono::duration<double> suma1 = chrono::duration<double>::zero();
-        //chrono::duration<double> vidurkis = chrono::duration<double>::zero();
-    //for (int j = 1; j <= test; j++){
-
+    for (int i = 1000; i <= 1000; i=i*10){
         chrono::duration<double> suma = chrono::duration<double>::zero();
         auto start = std::chrono::high_resolution_clock::now();
         FailoNuskaitymas(to_string(i) + ".txt", studentai, n);
         auto end = std::chrono::high_resolution_clock::now();
         chrono::duration<double> diff = end-start;
         suma += diff;
-        cout << i << " studentu sugeneravimas ir spausdinimas i faila: " << diff.count() << endl;
+        cout << i << " studentu nuskaitymas is failo: " << diff.count() << endl;
 
             start = std::chrono::high_resolution_clock::now();
-            sort(studentai.begin(), studentai.end(), sortDidejant); //SortVardas, sortDidejant, sortMazejant
+            sort(studentai.begin(), studentai.end(), sortingFunction);
             end = std::chrono::high_resolution_clock::now();
             diff = end - start;
             suma = suma + diff;
@@ -74,14 +77,7 @@ int main() {
 
         cout << i << " irasu testo laikas: " << suma.count() << endl;
         cout << endl;
-        //suma1 += suma;
      }
-    //vidurkis = suma1 / test;
-    //cout << i << " irasu testo laiku vidurkis: " << vidurkis.count() << endl;
-    //cout << endl;
-   // }
-
-
+//cout << "Studento objekto adresas Vector konteineryje: " << &studentai.back() << endl;
     return 0;
 }
-
