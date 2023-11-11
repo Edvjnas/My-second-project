a4194cac518eaf20750b15c724e5a246d74b0f01
Edvjnas <145874175+Edvjnas@users.noreply.github.com>
2023-11-11 16:22:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,101 +1,20 @@
 #include "mylib.h"
 
-
 using namespace std;
 
 int main() {
+    cout << "Pasirinkite konteinerio tipa (1 - vector, 2 - list): ";
+    int containerType;
+    cin >> containerType;
 
-
-    /*vector<Studentas> studentai;
-    IvestiDuomenis(studentai);*/
-
-
-    int test = 3;
-    list<Studentas> studentai;
-
-    int b;
-    int n;
-
-    cout << "Jei norite is naujo generuoti failus, rasykite 1: " 
-    cin >> b;
-
-    if (b==1){
-        cout << "Kiek namu darbu norite sugeneruoti? ";
-        cin >> n;
-     GeneruotiStudentus(1000, "1000.txt", n);
-     GeneruotiStudentus(10000, "10000.txt", n);
-     GeneruotiStudentus(100000, "100000.txt", n);
-     GeneruotiStudentus(1000000, "1000000.txt", n);
-     GeneruotiStudentus(10000000, "10000000.txt", n);
+    if (containerType == 1) {
+        vector<Studentas> studentai;
+        Testavimas(studentai);
+    } else if (containerType == 2) {
+        list<Studentas> studentai;
+        Testavimas(studentai);
     }
-
-    pair<list<Studentas>, list<Studentas>> sortedStudents = RikiuotiStudentus(studentai);
-
-    // vector
-    // 1000 irasu testu (3) vidurkis = 0.0260391
-    // 10000 irasu testu (3) vidurkis = 0.186274
-    // 100000 irasu testu (3) vidurkis = 1.90762
-    // 1000000 irasu testu (3) vidurkis = 22.9662
-    // 10000000 irasu testu (3) vidurkis = 161.129
-
-
-
-
-    for (int i = 1000; i <= 1000; i=i*10){
-        chrono::duration<double> suma1 = chrono::duration<double>::zero();
-        chrono::duration<double> vidurkis = chrono::duration<double>::zero();
-    //for (int j = 1; j <= test; j++){
-
-        chrono::duration<double> suma = chrono::duration<double>::zero();
-        auto start = std::chrono::high_resolution_clock::now();
-        FailoNuskaitymas(to_string(i) + ".txt", studentai, n);
-        auto end = std::chrono::high_resolution_clock::now();
-        chrono::duration<double> diff = end-start;
-        suma += diff;
-        cout << i << " studentu nuskaitymas is failo: " << diff.count() << endl;
-
-            start = std::chrono::high_resolution_clock::now();
-            studentai.sort(sortDidejant); //SortVardas, sortDidejant, sortMazejant
-            end = std::chrono::high_resolution_clock::now();
-            diff = end - start;
-            suma = suma + diff;
-            cout << i << " studentu rusiavimas pagal parametra: " << diff.count() << endl;
-
-                start = std::chrono::high_resolution_clock::now();
-                pair<list<Studentas>, list<Studentas>> sortedStudents = RikiuotiStudentus(studentai);
-                list<Studentas> genijai = sortedStudents.first;
-                list<Studentas> vargsiukai = sortedStudents.second;
-                end = std::chrono::high_resolution_clock::now();
-                diff = end - start;
-                suma = suma + diff;
-                cout << i << " studentu rusiavimas i dvi grupes: " << diff.count() << endl;
-
-                    start = std::chrono::high_resolution_clock::now();
-                    SpausdintiStudentus(genijai, "genijai.txt");
-                    end = std::chrono::high_resolution_clock::now();
-                    diff = end - start;
-                    suma = suma + diff;
-                    cout << i << " geniju spausdinimas i faila: " << diff.count() << endl;
-
-                        start = std::chrono::high_resolution_clock::now();
-                        SpausdintiStudentus(vargsiukai, "vargsiukai.txt");
-                        end = std::chrono::high_resolution_clock::now();
-                        diff = end - start;
-                        suma = suma + diff;
-                        cout << i << " vargsiuku spausdinimas i faila: " << diff.count() << endl;
-                        cout << endl;
-
-        cout << i << " irasu testo laikas: " << suma.count() << endl;
-        cout << endl;
-        //suma1 += suma;
-     }
-    //vidurkis = suma1 / test;
-    //cout << i << " irasu testo laiku vidurkis: " << vidurkis.count() << endl;
-    //cout << endl;
-   // }
-   
-   cout << "Studento objekto adresas List konteineryje: " << &studentai.back() << endl;
-
 
     return 0;
 }
+
