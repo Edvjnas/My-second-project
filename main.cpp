e69c1ee5ab134b71cc4969c441f6164cdb8466d0
Edvjnas <145874175+Edvjnas@users.noreply.github.com>
2023-11-21 22:52:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,28 @@
 using namespace std;
 
 int main() {
-    cout << "Pasirinkite konteinerio tipa (1 - vector, 2 - list): ";
+        int s;
+        cout << "Pasirinkite konteinerio tipa (1 - vector, 2 - list): ";
     int containerType;
     cin >> containerType;
-
-    if (containerType == 1) {
+        cout << "Ar norite nuskaityti is failo ir skaiciuoti laika?(Rasykite 2, jei taip, ir 1, jei ne)"<< endl;
+        cin >> s;
+    if (s == 1 && containerType == 1){
+        vector<Studentas> studentai;
+        IvestiDuomenis(studentai);
+    }
+    if (s == 2 && containerType == 1) {
         vector<Studentas> studentai;
         Testavimas(studentai);
-    } else if (containerType == 2) {
+    }
+    if (s == 1 && containerType == 2){
+        list<Studentas> studentai;
+        IvestiDuomenis(studentai);
+    }
+    if (s == 2 && containerType == 2) {
         list<Studentas> studentai;
         Testavimas(studentai);
     }
 
     return 0;
 }
-
